71cba8e73d7aea065d3453165720b0958b0bbbc4
krishmulls <murali.raajanthira@stud.uni-hannover.de>
2023-03-09 15:25:40 +0100

    Optimised Parameter Passing and using base code instead of event based logging

--- src/syncFolders.py
+++ src/syncFolders.py
@@ -1,7 +1,5 @@
 import os
 from filehash import FileHash
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 import logging
 import time
 import trio
@@ -12,7 +10,7 @@ def logInitializer(logPath = None):
         logger.setLevel(logging.INFO)
         log_streamer = logging.StreamHandler()
         format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s'
-        formatter = logging.Formatter(format)
+        formatter = logging.Formatter(format, datefmt='%Y-%m-%d %H:%M:%S',)
         log_streamer.setFormatter(formatter)
         logger.addHandler(log_streamer)
         if logPath != None:
@@ -21,26 +19,12 @@ def logInitializer(logPath = None):
             logger.addHandler(log_handler)
         return logger
 
-class Handler(FileSystemEventHandler):
-    def on_created(self, event):
-        logger = logging.getLogger( __name__ )
-        logger.info(event.src_path)
-        # Event is created, you can process it now
-    
-    def on_deleted(self, event):
-        logger = logging.getLogger( __name__ )
-        logger.info(event.src_path)
-
-    def on_moved(self, event):
-        logger = logging.getLogger( __name__ )
-        logger.info(event.src_path)
-
 class folderSync():
 
     def __init__(self):
         self.sha2hasher = FileHash('sha256')
 
-    def folderDictGeneration(self, filepath):
+    def fileHashDictGeneration(self, filepath):
         files = {}
         for dirpath, dirname, filenames in os.walk(filepath):
             for file in filenames:
@@ -51,71 +35,71 @@ class folderSync():
                 files[filekey] = self.sha2hasher.cathash_dir(os.path.join(dirpath, dir), pattern = '*')
         return files
     
-    def copyFileSourceToDestination(self, sourceFolder, replicaFolder, changeList):
+    def copyFileSourceToDestination(self, changeList):
         for files in changeList:
-            if os.path.isfile(sourceFolder + files):
+            if os.path.isfile(self.sourceFolder + files):
                 fileDirectory = files.rsplit('\\',1)[0]
                 try:
-                    shutil.copy2((sourceFolder + files), (replicaFolder + files))
+                    shutil.copy2((self.sourceFolder + files), (self.replicaFolder + files))
                 except:
-                    os.mkdir(replicaFolder+fileDirectory)
-                    shutil.copy2((sourceFolder + files), (replicaFolder + files))
+                    os.mkdir(self.replicaFolder+fileDirectory)
+                    shutil.copy2((self.sourceFolder + files), (self.replicaFolder + files))
             else:
-                check = os.path.exists(replicaFolder+files)
+                check = os.path.exists(self.replicaFolder+files)
                 if not check:
-                    os.mkdir(replicaFolder+files)
+                    os.mkdir(self.replicaFolder+files)
+
+    def deleteFolderFiles(self):
+        for sKey in self.replicaFiles: 
+            if sKey not in self.sourceFiles and os.path.exists(self.replicaFolder + sKey): 
+                try:
+                    path = self.replicaFolder + sKey
+                    # os.chmod(path, 0o777)  # uncomment it if you want to run with admin rights
+                    if os.path.isfile(path):
+                        os.remove(path)
+                        self.logger.info("Removed a file %s", path)
+                    else:
+                        shutil.rmtree(path)
+                        self.logger.info("Removed a folder %s", path)
+                except:
+                    self.logger.error("You dont have permission to delete and therefore the folders are not sync")
     
     async def folderComparison(self, sourceFolder, replicaFolder, interval,  logPath):
-        logger = logInitializer(logPath)
-        event_handler = Handler()
-        observer = Observer()
-        observer.schedule(event_handler, sourceFolder, recursive=True)
-        observer.start()
-        traceFiles = {}
-        try:
-            while True:
-                modifiedList = []
-                notFoundList = []
-                time.sleep(interval)
-                sourceFiles = self.folderDictGeneration(sourceFolder)
-                replicaFiles = self.folderDictGeneration(replicaFolder)
-                
-                for sKey in sourceFiles:
-                    if sKey in replicaFiles:
-                        if sourceFiles[sKey] != replicaFiles[sKey]:
-                            modifiedList.append(sKey)
-                    else:
-                        notFoundList.append(sKey)
-
-                if modifiedList == [] and notFoundList == [] and sourceFiles == traceFiles:
-                    logger.info('Folder Synced')
+        self.logger = logInitializer(logPath)
+        self.sourceFolder = sourceFolder
+        self.replicaFolder = replicaFolder
+        while True:
+            modifiedList = []
+            notFoundList = []
+            time.sleep(interval)
+            self.sourceFiles = self.fileHashDictGeneration(self.sourceFolder)
+            self.replicaFiles = self.fileHashDictGeneration(self.replicaFolder)
+            
+            for sKey in self.sourceFiles:
+                if sKey in self.replicaFiles:
+                    if self.sourceFiles[sKey] != self.replicaFiles[sKey]:
+                        modifiedList.append(sKey)
                 else:
-                    logger.info('Folder Not Synced')
-                    if modifiedList !=[]:
-                        self.copyFileSourceToDestination(sourceFolder, replicaFolder, modifiedList)
-                    if notFoundList != []:
-                        self.copyFileSourceToDestination(sourceFolder, replicaFolder, notFoundList)
-                    if sourceFiles != traceFiles:
-                        for sKey in traceFiles:
-                            if sKey not in sourceFiles: 
-                                if os.path.exists(replicaFolder + sKey):
-                                    try:
-                                        path = replicaFolder + sKey
-                                        # os.chmod(path, 0o777)  # comment it if you dont want to run with admin rights
-                                        shutil.rmtree(path)
-                                    except:
-                                        logger.error("You dont have permission to delete and therefore the folders are not sync")
-                    traceFiles.clear()
-                    traceFiles = sourceFiles
-        finally:
-            observer.stop()
-            observer.join()
+                    notFoundList.append(sKey)
+
+            if modifiedList == [] and notFoundList == [] and self.sourceFiles == self.replicaFiles: 
+                self.logger.info('Folder Synced')
+            else:
+                self.logger.info('Folder Not Synced')
+                if modifiedList !=[]:
+                    self.copyFileSourceToDestination( modifiedList)
+                if notFoundList != []:
+                    self.copyFileSourceToDestination( notFoundList)
+                if self.sourceFiles != self.replicaFiles: 
+                    self.deleteFolderFiles()
+
+
         
 
 if __name__ == '__main__':
     sync_obj = folderSync()
-    sourceFolder = r"D:\Workspace\Veeam\example\sourceFolder"
-    replicaFolder = r"D:\Workspace\Veeam\example\replicaFolder"
+    sourceFolder = r"D:\workspace\Veeam\example\sourceFolder"
+    replicaFolder = r"D:\workspace\Veeam\example\replicaFolder"
     syncTime = 5
-    logPath = "D:\Workspace\Veeam"
+    logPath = "D:\workspace\Veeam"
     trio.run(sync_obj.folderComparison, sourceFolder, replicaFolder, syncTime, logPath)
